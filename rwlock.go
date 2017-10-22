f0a338a348593c94a07f26ec632336c97238ed71
maziang <maziang@bytedance.com>
2017-10-22 15:57:49 +0800

    bug fix

--- rwlock.go
+++ rwlock.go
@@ -1,34 +1,47 @@
 // 经典正例
 package main
 
-import "time"
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 var sum int
 var rw sync.RWMutex
 
-func read()  {}
-func write() {}
+func read(id int) {
+	time.Sleep(time.Millisecond)
+	fmt.Printf("%d Read\n", id)
+}
+func write() {
+	time.Sleep(time.Millisecond)
+	fmt.Println("Write")
+}
 
-func reader() {
-	for i := 0; i < 100; i++ {
+func reader(id int) {
+	for i := 0; i < 10; i++ {
 		rw.RLock()
-		read()
+		fmt.Printf("%d Acquire\n", id)
+		read(id)
 		rw.RUnlock()
+		fmt.Printf("%d Release\n", id)
 	}
 }
 func writer() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
 		rw.Lock()
+		fmt.Println("Writer Acquire")
 		write()
-		rw.RLock()
+		rw.Unlock()
+		fmt.Println("Writer Release")
 	}
 }
 
 func main() {
 	sum = 0
-	for i := 0; i < 10; i++ {
-		go reader()
+	for i := 0; i < 3; i++ {
+		go reader(i)
 	}
 	go writer()
 	time.Sleep(time.Second) // 主进程阻塞一秒
